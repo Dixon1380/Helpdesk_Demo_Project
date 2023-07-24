d6b5fa50dafb891b6ca70bc039d7ccf2ecdc394d
David Dixon <DixonDavid1332@gmail.com>
2023-07-23 20:56:14 -0500

    Revert "Add files to project"
    This reverts commit 4d4a991246d41bd85a36d7030480041c1505c0e3.

--- ticketing/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import TicketListView, TicketDetailView
-
-urlpatterns = [
-    path('tickets/', TicketListView.as_view(), name='ticket-list'),
-    path('tickets/<int:pk>/', TicketDetailView.as_view(), name='ticket-detail'),
-]
